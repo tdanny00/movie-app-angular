1b2a773023c5c5714a0b55f7d0e08c5ea95ecb9a
tdanny00 <tolgyesipontdani@gmail.com>
2022-06-21 23:17:12 +0200

    login page design v2 - updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule} from "@angular/forms";
+import { ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('')
+  })
 
-  ngOnInit(): void {
+  get email() {
+    return this.loginForm.get('email')
   }
 
 }
